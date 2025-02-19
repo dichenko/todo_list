0e38f96d1a12ece20b19e15b7cea7e2e02221eb2
Dinechko <m@dichenko.ru>
2025-02-19 09:54:10 +0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ActiveTasks from '@/views/ActiveTasks.vue'
 import DeletedTasks from '@/views/DeletedTasks.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'active',
-    component: ActiveTasks
-  },
-  {
-    path: '/deleted',
-    name: 'deleted',
-    component: DeletedTasks
-  }
-]
-
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'active',
+      component: ActiveTasks
+    },
+    {
+      path: '/deleted',
+      name: 'deleted',
+      component: DeletedTasks
+    }
+  ]
 })
 
 export default router 
